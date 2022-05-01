7d5e3d338649f9f1db6fa7acdec7e7119a58a212
Mykhailo Kostiuchenko <mykhailo.kostiuchenko@e-hps.com>
2022-05-01 23:56:54 +0300

    added catalog api

--- src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Catalog.API.Data;
 using Catalog.API.Entities;
 using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,6 +10,11 @@ namespace Catalog.API.Repositories
     public class ProductRepository : IProductRepository
     {
         private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
 
         public async Task<IEnumerable<Product>> GetProducts()
         {
